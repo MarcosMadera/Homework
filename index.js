e9fe448865e78a5e5d0d2ada051f9f8c0b5affb4
MarcosMadera <CodingRules66@gmail.com>
2024-12-01 16:19:50 -0600

    homework week2 part2

---
+++ index.js
@@ -0,0 +1,20 @@
+const pizzaPlace = "That's amore!";
+const numberOfToppings = 12;
+
+console.log("Pizza Place Name", pizzaPlace, "- Type", typeof pizzaPlace);
+console.log(
+  "Number of Toppings:",
+  numberOfToppings,
+  "- Type",
+  typeof numberOfToppings
+);
+
+console.log(
+  `Welcome to ${pizzaPlace}, where we are proud to offer ${numberOfToppings} mouth-watering toppings to choose from!`
+);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A Whole lot of pizza.");
+}
