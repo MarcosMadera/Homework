493cef2a84ce3aa0f7e5d74f99ef695fed5235cf
MarcosMadera <CodingRules66@gmail.com>
2024-12-13 16:19:58 -0600

    redohomeworkweek3

--- Week3/pizzaPlaceweek3.js
+++ Week3/pizzaPlaceweek3.js
@@ -1,35 +1,41 @@
 let pizzaToppings = ["pepperoni", "bacon", "mushrooms", "onions"];
 
-let index = 0;
-
 function greetCustomer() {
-  let greet = ("Welcome to Pizza House, our toppings are:");
+  let greet = "Welcome to Pizza House, our toppings are: ";
   for (let topping of pizzaToppings) {
-    greet += topping + ", ";
+    greet += `${topping}, `;
   }
+  console.log(greet);
 }
+greetCustomer();
 
-function getPizzaOrder(size, crust, toppings) {
-  let order = `One ${size} ${crust} crust pizza with ${toppings(", ")} coming up!`;
-console.log(order);
-let orderArray =  [size, crust, toppings];
-return orderArray;
-}
-
-function preparePizza(order) {
-  console.log("Cooking pizza");
-}
-  let pizza = {
-    size: order[0],
-    crust: order[1],
-    toppings: order[2]
-  };
+function getPizzaOrder(size, crust, ...toppings) {
+  let order = `One ${size} ${crust} crust pizza with `;
+  for (let topping of toppings) {
+    order += `${topping}`;
+  }
+  order += ", ... coming up!";
   console.log(order);
 
-  return pizza;
-
-function servePizza(pizza) {
-  console.log(`Order up! Here's your ${pizza.size} ${pizza.crust}crust pizza with ${pizza.toppings(", ")}. Enjoy!`
-  );
+  return [size, crust, toppings];
 }
 
+function preparePizza([size, crust, toppings]) {
+  console.log("Cooking pizza");
+  return {
+    size: size,
+    crust: crust,
+    toppings: toppings
+  };
+}
+
+function servePizza(pizza) {
+  let message = `Order up! Here's your ${pizza.size}  ${pizza.crust} crust pizza with `;
+  for (let topping of pizza.toppings) {
+    message += `${topping}`;
+  }
+  message += "... Enjoy!";
+  console.log(message);
+  return pizza;
+}
+servePizza(preparePizza(getPizzaOrder("large", "thin", "pepperoni")));
